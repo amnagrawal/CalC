3f947fd4dc5ba7e5a586f885474d8fd225e74918
amnagrawal <amnagrawal1994@gmail.com>
2015-03-30 18:42:09 +0530

    Basic UI designed!

---
+++ app/src/main/java/com/example/aman/calc/Fragment_main.java
@@ -0,0 +1,20 @@
+package com.example.aman.calc;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by aman on 30/3/15.
+ */
+public class Fragment_main extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
